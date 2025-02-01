e5f81ba8551ab0f0dcf09df6005f9c4d8be698e9
Neil Molina <neilmolinarellano04@gmail.com>
2025-02-01 16:39:30 +0100

    Inicio del proyecto web

--- Scripts-BD/BD.sql
+++ Scripts-BD/BD.sql
@@ -2,70 +2,70 @@
 DROP DATABASE IF EXISTS proyecto_tfg_daw;
 
 -- Crear base de datos
-CREATE DATABASE IF NOT EXISTS proyecto_tfg_daw;
-use proyecto_tfg_daw;
+CREATE DATABASE proyecto_tfg_daw;
+\c proyecto_tfg_daw;
 
 -- Crear tabla roles
 CREATE TABLE IF NOT EXISTS roles (
-	idRol INT NOT NULL auto_increment primary key,
-    rol varchar(45)
+    idRol SERIAL PRIMARY KEY,
+    rol VARCHAR(45)
 );
 
 INSERT INTO roles (rol) 
-VALUES ("usuario"), ("admin");
+VALUES ('usuario'), ('admin');
 SELECT * FROM roles;
 
 -- Crear tabla usuarios
 CREATE TABLE IF NOT EXISTS usuarios (
-	idUsuario  CHAR(36) NOT NULL PRIMARY KEY,
-    nombreUsuario varchar(45) unique,
-    email varchar(100) unique,
-    password varchar(256),
-    urlImagen varchar(255) unique,	
+    idUsuario UUID PRIMARY KEY,
+    nombreUsuario VARCHAR(45) UNIQUE,
+    email VARCHAR(100) UNIQUE,
+    password VARCHAR(256),
+    urlImagen VARCHAR(255) UNIQUE,    
     idRol INT DEFAULT 1,
-    CONSTRAINT idRol foreign key (idRol) REFERENCES roles(idRol)
+    CONSTRAINT fk_idRol FOREIGN KEY (idRol) REFERENCES roles(idRol)
 );
 
 INSERT INTO usuarios (idUsuario, nombreUsuario, email, password, urlImagen, idRol)
 VALUES 
-    ('b6acb1ec-cea8-11ef-a47b-a85e45600cdd', 'admin', 'admin@example.com', SHA2('adminpass789', 256), 'http://example.com/admin1.jpg', 2),
-    ('b6acb358-cea8-11ef-a47b-a85e45600cdd', 'neil', 'neil@example.com', SHA2('password123', 256), 'http://example.com/usuario1.jpg', 1),
-    ('b6acb3f8-cea8-11ef-a47b-a85e45600cdd', 'pepe', 'pepe@example.com', SHA2('mypassword456', 256), 'http://example.com/usuario2.jpg', 1);
+    ('b6acb1ec-cea8-11ef-a47b-a85e45600cdd', 'admin', 'admin@example.com', digest('adminpass789', 'sha256')::text, 'http://example.com/admin1.jpg', 2),
+    ('b6acb358-cea8-11ef-a47b-a85e45600cdd', 'neil', 'neil@example.com', digest('password123', 'sha256')::text, 'http://example.com/usuario1.jpg', 1),
+    ('b6acb3f8-cea8-11ef-a47b-a85e45600cdd', 'pepe', 'pepe@example.com', digest('mypassword456', 'sha256')::text, 'http://example.com/usuario2.jpg', 1);
 -- Consulta select usuarios:
 SELECT  
-	u.idUsuario, 
+    u.idUsuario, 
     u.nombreUsuario, 
     u.email,
     u.password,
-    u.urLImagen, 
+    u.urlImagen, 
     r.rol
-    FROM usuarios u
+FROM usuarios u
 INNER JOIN roles r 
 ON u.idRol = r.idRol;
 
--- Diarios
+-- Crear tabla diarios
 CREATE TABLE IF NOT EXISTS diarios(
-	idDiario CHAR(36) NOT NULL PRIMARY KEY,
-    titulo varchar(45),
+    idDiario UUID PRIMARY KEY,
+    titulo VARCHAR(45),
     descripcion TEXT,
     fechaActividad TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-    idUsuario CHAR(36),
-    CONSTRAINT idUsuarioDiario FOREIGN KEY (idUsuario) REFERENCES usuarios(idUsuario)
+    idUsuario UUID,
+    CONSTRAINT fk_idUsuarioDiario FOREIGN KEY (idUsuario) REFERENCES usuarios(idUsuario)
 );
 
 -- Insertar registros en la tabla diarios
 INSERT INTO diarios (idDiario, titulo, descripcion, fechaActividad, idUsuario)
 VALUES
-    (UUID(), 'Diario de Admin', 'Este es el primer diario creado por el administrador.', NOW(), 'b6acb1ec-cea8-11ef-a47b-a85e45600cdd'),
-    (UUID(), 'Viaje a la montaña', 'Descripción del viaje a la montaña realizado por Neil.', NOW(), 'b6acb358-cea8-11ef-a47b-a85e45600cdd'),
-    (UUID(), 'Entrenamiento diario', 'Registro del entrenamiento diario de Pepe.', NOW(), 'b6acb3f8-cea8-11ef-a47b-a85e45600cdd'),
-    (UUID(), 'Reunión de equipo', 'Admin registró las notas de la reunión de equipo.', NOW(), 'b6acb1ec-cea8-11ef-a47b-a85e45600cdd'),
-    (UUID(), 'Diario personal', 'Neil escribió reflexiones personales en su diario.', NOW(), 'b6acb358-cea8-11ef-a47b-a85e45600cdd'),
-    (UUID(), 'Sesión de lectura', 'Pepe registró los libros leídos en su sesión diaria.', NOW(), 'b6acb3f8-cea8-11ef-a47b-a85e45600cdd');
+    (gen_random_uuid(), 'Diario de Admin', 'Este es el primer diario creado por el administrador.', NOW(), 'b6acb1ec-cea8-11ef-a47b-a85e45600cdd'),
+    (gen_random_uuid(), 'Viaje a la montaña', 'Descripción del viaje a la montaña realizado por Neil.', NOW(), 'b6acb358-cea8-11ef-a47b-a85e45600cdd'),
+    (gen_random_uuid(), 'Entrenamiento diario', 'Registro del entrenamiento diario de Pepe.', NOW(), 'b6acb3f8-cea8-11ef-a47b-a85e45600cdd'),
+    (gen_random_uuid(), 'Reunión de equipo', 'Admin registró las notas de la reunión de equipo.', NOW(), 'b6acb1ec-cea8-11ef-a47b-a85e45600cdd'),
+    (gen_random_uuid(), 'Diario personal', 'Neil escribió reflexiones personales en su diario.', NOW(), 'b6acb358-cea8-11ef-a47b-a85e45600cdd'),
+    (gen_random_uuid(), 'Sesión de lectura', 'Pepe registró los libros leídos en su sesión diaria.', NOW(), 'b6acb3f8-cea8-11ef-a47b-a85e45600cdd');
 
 -- Consulta select de diarios:
 SELECT 
-	d.idDiario,
+    d.idDiario,
     d.titulo,
     d.descripcion,
     d.fechaActividad,
@@ -76,11 +76,11 @@ ON d.idUsuario = u.idUsuario;
 
 -- Crear tipos
 CREATE TABLE IF NOT EXISTS tipos(
-	idTipo INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    idTipo SERIAL PRIMARY KEY,
     tipo VARCHAR(45),
     color VARCHAR(45),
-    idUsuario CHAR(36),
-    CONSTRAINT idUsuarioTipo FOREIGN KEY (idUsuario) REFERENCES usuarios(idUsuario)
+    idUsuario UUID,
+    CONSTRAINT fk_idUsuarioTipo FOREIGN KEY (idUsuario) REFERENCES usuarios(idUsuario)
 );
 
 -- Insertar tipos de tareas con colores en formato hexadecimal
@@ -95,8 +95,8 @@ SELECT * FROM tipos;
 
 -- Crear estados
 CREATE TABLE IF NOT EXISTS estados(
-	idEstado INT NOT NULL auto_increment PRIMARY KEY,
-    estado varchar(45)
+    idEstado SERIAL PRIMARY KEY,
+    estado VARCHAR(45)
 );
 -- Insertar estados de tareas 
 INSERT INTO estados (estado)
@@ -106,18 +106,17 @@ SELECT * FROM estados;
 
 -- Crear tareas
 CREATE TABLE IF NOT EXISTS tareas(
-    idTarea CHAR(36) PRIMARY KEY,
+    idTarea UUID PRIMARY KEY,
     titulo VARCHAR(45),
     descripcion VARCHAR(255),
     fechaActividad TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
     idEstado INT,
     idTipo INT,
-    idUsuario CHAR(36),
-    CONSTRAINT idEstadoTarea FOREIGN KEY (idEstado) REFERENCES estados(idEstado),
-    CONSTRAINT idTipoTarea FOREIGN KEY (idTipo) REFERENCES tipos(idTipo),
-    CONSTRAINT idUsuarioTarea FOREIGN KEY (idUsuario) REFERENCES usuarios(idUsuario) -- Corregido el nombre de la tabla 'usuario' a 'usuarios'
+    idUsuario UUID,
+    CONSTRAINT fk_idEstadoTarea FOREIGN KEY (idEstado) REFERENCES estados(idEstado),
+    CONSTRAINT fk_idTipoTarea FOREIGN KEY (idTipo) REFERENCES tipos(idTipo),
+    CONSTRAINT fk_idUsuarioTarea FOREIGN KEY (idUsuario) REFERENCES usuarios(idUsuario)
 );
-
 
 -- Insertar tareas
 INSERT INTO tareas (idTarea, titulo, descripcion, fechaActividad, idEstado, idTipo, idUsuario)
@@ -129,14 +128,14 @@ VALUES
 
 -- Verificar los datos insertados
 SELECT 
-ta.idTarea,
-ta.titulo,
-ta.descripcion,
-ta.fechaActividad,
-e.idEstado,
-e.estado,
-ti.tipo,
-u.nombreUsuario
+    ta.idTarea,
+    ta.titulo,
+    ta.descripcion,
+    ta.fechaActividad,
+    e.idEstado,
+    e.estado,
+    ti.tipo,
+    u.nombreUsuario
 FROM tareas ta
 INNER JOIN estados e
 ON ta.idEstado = e.idEstado
@@ -147,12 +146,12 @@ ON ta.idUsuario = u.idUsuario;
 
 -- Crear tabla amigos
 CREATE TABLE IF NOT EXISTS amigos(
-	idPrimerUsuario CHAR(36),
-    idSegundoUsuario CHAR(36),
+    idPrimerUsuario UUID,
+    idSegundoUsuario UUID,
     solicitudAmigoAceptada BOOLEAN DEFAULT FALSE,
     
-    CONSTRAINT idPrimerUsuarioAmigo FOREIGN KEY (idPrimerUsuario) REFERENCES usuarios(idUsuario),
-    CONSTRAINT idSegundorUsuarioAmigo FOREIGN KEY (idSegundoUsuario) REFERENCES usuarios(idUsuario)
+    CONSTRAINT fk_idPrimerUsuarioAmigo FOREIGN KEY (idPrimerUsuario) REFERENCES usuarios(idUsuario),
+    CONSTRAINT fk_idSegundoUsuarioAmigo FOREIGN KEY (idSegundoUsuario) REFERENCES usuarios(idUsuario)
 );
 
 INSERT INTO amigos (idPrimerUsuario, idSegundoUsuario, solicitudAmigoAceptada)
@@ -162,27 +161,27 @@ VALUES
     ('b6acb358-cea8-11ef-a47b-a85e45600cdd', 'b6acb3f8-cea8-11ef-a47b-a85e45600cdd', TRUE); -- neil y pepe son amigos
     
 SELECT 
-a.idPrimerUsuario,
-u1.nombreUsuario,
-a.idSegundoUsuario,
-u2.nombreUsuario,
-a.solicitudAmigoAceptada
+    a.idPrimerUsuario,
+    u1.nombreUsuario,
+    a.idSegundoUsuario,
+    u2.nombreUsuario,
+    a.solicitudAmigoAceptada
 FROM amigos a
 INNER JOIN usuarios u1
 ON a.idPrimerUsuario = u1.idUsuario
 INNER JOIN usuarios u2
 ON a.idSegundoUsuario = u2.idUsuario;
 
--- Crear tabla amigos
+-- Crear tabla amigos_has_tareas
 CREATE TABLE IF NOT EXISTS amigos_has_tareas(
-	idPrimerUsuario CHAR(36),
-    idSegundoUsuario CHAR(36),
-    idTarea CHAR(36),
+    idPrimerUsuario UUID,
+    idSegundoUsuario UUID,
+    idTarea UUID,
     solicitudTareaAceptada BOOLEAN DEFAULT FALSE,
     
-    CONSTRAINT idPrimerUsuarioTarea FOREIGN KEY (idPrimerUsuario) REFERENCES usuarios(idUsuario),
-    CONSTRAINT idSegundorUsuarioTarea FOREIGN KEY (idSegundoUsuario) REFERENCES usuarios(idUsuario),
-    CONSTRAINT idTareaTarea FOREIGN KEY (idTarea) REFERENCES tareas(idTarea)
+    CONSTRAINT fk_idPrimerUsuarioTarea FOREIGN KEY (idPrimerUsuario) REFERENCES usuarios(idUsuario),
+    CONSTRAINT fk_idSegundoUsuarioTarea FOREIGN KEY (idSegundoUsuario) REFERENCES usuarios(idUsuario),
+    CONSTRAINT fk_idTareaTarea FOREIGN KEY (idTarea) REFERENCES tareas(idTarea)
 );
 
 INSERT INTO amigos_has_tareas (idPrimerUsuario, idSegundoUsuario, idTarea, solicitudTareaAceptada)
@@ -191,26 +190,23 @@ VALUES
     ('b6acb358-cea8-11ef-a47b-a85e45600cdd', 'b6acb3f8-cea8-11ef-a47b-a85e45600cdd', 'a992981e-cf6f-11ef-9f16-a85e45600cdd', TRUE); -- neil y pepe comparten Evento de lanzamiento.
     
 SELECT 
-aht.idPrimerUsuario as nombrePrimerUsuario,
-u1.nombreUsuario,
-aht.idSegundoUsuario,
-u2.nombreUsuario as nombreSegundoUsuario,
-aht.solicitudTareaAceptada,
-ta.idTarea,
-u3.nombreUsuario as creador,
-ta.titulo,
-ta.descripcion,
-ta.fechaActividad,
-e.estado,
-ti.tipo
-FROM 
-amigos_has_tareas aht
-
+    aht.idPrimerUsuario as nombrePrimerUsuario,
+    u1.nombreUsuario,
+    aht.idSegundoUsuario,
+    u2.nombreUsuario as nombreSegundoUsuario,
+    aht.solicitudTareaAceptada,
+    ta.idTarea,
+    u3.nombreUsuario as creador,
+    ta.titulo,
+    ta.descripcion,
+    ta.fechaActividad,
+    e.estado,
+    ti.tipo
+FROM amigos_has_tareas aht
 INNER JOIN usuarios u1
 ON aht.idPrimerUsuario = u1.idUsuario
 INNER JOIN usuarios u2
 ON aht.idSegundoUsuario = u2.idUsuario
-
 INNER JOIN tareas ta
 ON aht.idTarea = ta.idTarea
 INNER JOIN estados e
---
+++ proyect/server/src/index.ts
@@ -0,0 +1,20 @@
+import express, { Express, Request, Response } from "express";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+const app: Express = express();
+const port = process.env.PORT || 3000;
+
+app.get("/", (req: Request, res: Response) => {
+  const { name } = req.query;
+  if (name) {
+    res.send(`Hello ${name}`);
+  } else {
+    res.send("Express + TypeScript Server Pepe");
+  }
+});
+
+app.listen(port, () => {
+  console.log(`[server]: Server is running at http://localhost:${port}`);
+});
