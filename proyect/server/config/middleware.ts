35fc89a9e557a151aac4e443a4bcb7e642a416ee
Neil Molina <neilmolinarellano04@gmail.com>
2025-06-05 16:08:45 +0200

    Finished deployment of frontend

--- proyect/server/config/middleware.ts
+++ proyect/server/config/middleware.ts
@@ -1,12 +1,21 @@
 import { Request, Response, NextFunction } from "express";
 
-// Middleware para manejo de CORS
+const allowedOrigins = [
+  "http://localhost:5173",
+  "http://tfg-daw-frontend.s3-website.eu-north-1.amazonaws.com",
+];
+
 export const corsMiddleware = (
   req: Request,
   res: Response,
   next: NextFunction
 ): void => {
-  res.setHeader("Access-Control-Allow-Origin", "http://localhost:5173");
+  const origin = req.headers.origin as string;
+
+  if (allowedOrigins.includes(origin)) {
+    res.setHeader("Access-Control-Allow-Origin", origin);
+  }
+
   res.setHeader("Access-Control-Allow-Credentials", "true");
   res.setHeader(
     "Access-Control-Allow-Methods",
@@ -18,20 +27,9 @@ export const corsMiddleware = (
   );
 
   if (req.method === "OPTIONS") {
-    res.sendStatus(200); // Responder a las solicitudes preflight
+    res.sendStatus(200);
     return;
   }
 
-  next(); // Continuar con la ejecución del siguiente middleware
-};
-
-// Middleware para manejo de errores generales
-export const errorHandler = (
-  err: Error,
-  req: Request,
-  res: Response,
-  next: NextFunction
-): void => {
-  console.error("Error:", err);
-  res.status(500).json({ error: "Internal Server Error" });
+  next();
 };
